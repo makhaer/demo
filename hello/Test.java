df98e122ee0c7e7f8bf9dbe6f21432aae20865a3
Mohammad Abul Khaer <khaer.mohammad@gmail.com>
2012-10-22 13:30:54 +0600

    Revert "modifying Test java"
    This reverts commit a0fb9113adc42a0ece24a4377ec15819f222984a.

--- hello/Test.java
+++ hello/Test.java
@@ -1,8 +1,7 @@
 public class Test{
 
- public static void main(String args[]){
-   System.out.println("Hello World !");
-   System.out.println("Welcome..");
- }
+public static void main(String args[]){
+  System.out.println("Hello World !");
+}
 
 }
